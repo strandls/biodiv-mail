d221a1a9cb6eb7db55a49d1da41196ec5c4c03d5
akrudra <abhishek.rudra02@gmail.com>
2021-02-02 14:33:48 +0530

    mail portal name issue fixed

--- src/main/java/com/strandls/mail/service/impl/UserMailServiceImpl.java
+++ src/main/java/com/strandls/mail/service/impl/UserMailServiceImpl.java
@@ -1,11 +1,13 @@
 package com.strandls.mail.service.impl;
 
 import java.util.List;
+import java.util.Properties;
 
 import javax.inject.Inject;
 
 import com.strandls.mail.model.MailInfo;
 import com.strandls.mail.service.UserMailService;
+import com.strandls.mail.util.PropertyFileUtil;
 import com.strandls.mail.util.ThreadUtil;
 
 import freemarker.template.Configuration;
@@ -15,19 +17,22 @@ public class UserMailServiceImpl implements UserMailService {
 	@Inject
 	private Configuration configuration;
 
+	private Properties props = PropertyFileUtil.fetchProperty("config.properties");
+	String portalName = props.getProperty("siteName");
+
 	@Override
 	public void sendActivationMail(List<MailInfo> info) {
-		ThreadUtil.startThread(configuration, "auth.ftlh", "Activate your account with India Biodiversity Portal", info);
+		ThreadUtil.startThread(configuration, "auth.ftlh", "Activate your account with " + portalName, info);
 	}
 
 	@Override
 	public void sendWelcomeMail(List<MailInfo> info) {
-		ThreadUtil.startThread(configuration, "welcome.ftlh", "Welcome to India Biodiversity Portal", info);
+		ThreadUtil.startThread(configuration, "welcome.ftlh", "Welcome to " + portalName, info);
 	}
 
 	@Override
 	public void sendResetPasswordMail(List<MailInfo> info) {
-		ThreadUtil.startThread(configuration, "auth.ftlh", "Reset password with India Biodiversity Portal", info);
+		ThreadUtil.startThread(configuration, "auth.ftlh", "Reset password with " + portalName, info);
 	}
 
 }
