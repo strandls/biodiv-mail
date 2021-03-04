1f5863ba4e2fadb5facd8882af7e7678487f5733
akrudra <abhishek.rudra02@gmail.com>
2021-03-04 16:34:09 +0530

    mail data modified

--- src/main/java/com/strandls/mail/util/ThreadUtil.java
+++ src/main/java/com/strandls/mail/util/ThreadUtil.java
@@ -11,6 +11,7 @@ import freemarker.template.Configuration;
 
 public class ThreadUtil {
 
+	@SuppressWarnings("unchecked")
 	public static void startThread(Configuration configuration, String templateFile, String mailSubject,
 			List<MailInfo> recipients) {
 		TemplateUtil template = new TemplateUtil(configuration);
@@ -20,18 +21,26 @@ public class ThreadUtil {
 			for (MailInfo info : recipients) {
 
 				Map<String, Object> data = info.getData();
+				System.out.println("Before manipulation");
 				System.out.println(info.toString());
 
 				data.put("siteName", PropertyFileUtil.fetchProperty("config.properties", "siteName"));
 				data.put("serverUrl", PropertyFileUtil.fetchProperty("config.properties", "serverUrl"));
-				String iconUrl = data.get("whatPosted.icon").toString();
+
+				Map<String, Object> whatPosted = (Map<String, Object>) data.get("whatPosted");
+				String iconUrl = whatPosted.get("icon").toString();
 				iconUrl = iconUrl.replace("_th1.", ".");
-				data.put("whatPosted.icon", iconUrl);
-				String userIcon = data.get("whoPosted.icon").toString();
+				whatPosted.put("icon", iconUrl);
+				data.put("whatPosted", whatPosted);
+
+				Map<String, Object> whoPosted = (Map<String, Object>) data.get("whoPosted");
+				String userIcon = whoPosted.get("icon").toString();
 				userIcon = userIcon.replace("_gall_th.", ".");
-				data.put("whoPosted.icon", userIcon);
+				whoPosted.put("icon", userIcon);
+				data.put("whoPosted", whoPosted);
 
 				info.setData(data);
+				System.out.println("After manipulation");
 				System.out.println(info.toString());
 
 				content = template.getTemplateAsString(templateFile, data);
